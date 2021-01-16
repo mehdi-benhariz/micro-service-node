78506ab7c3e00f8ceb81733575c26d977de8b51e
mehdi-benhariz <benharizmehdi20@gmail.com>
2021-01-16 20:58:10 +0000

    linking server1 with index

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const {getAll,getOne,create} = require('../controllers/bookController')
+const {getAll,getOne,create,remove,update} = require('../controllers/bookController')
 
 router.get('/all', getAll)
 router.get('/one',getOne)
 router.post('/',create)
+router.delete('/',remove)
+router.put('/',update)
 
 module.exports= router;
